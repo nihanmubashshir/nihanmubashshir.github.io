82b7f8b5e680a359dd66c6d3fae00ac6540493d8
Nihan <nihanmubashshir@gmail.com>
2023-08-14 04:31:06 +0600

    error fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'nihanmubashshit.github.io',
+  site: 'https://nihanmubashshit.github.io',
   integrations: [mdx(), sitemap(), tailwind()]
 });
