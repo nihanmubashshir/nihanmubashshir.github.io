6a07f2f9e937f361dcac6d558d10553e5d7b18fe
Nihan <nihanmubashshir@gmail.com>
2023-08-14 04:07:48 +0600

    project docs linked

---
+++ src/content/projects/start.md
@@ -0,0 +1,17 @@
+---
+
+title: Starting test
+---
+
+
+## MD
+I would like to understand how it works and how it should work in general.
+## Hope I can gain something out of it.
+
+```js
+class system(){
+    cosntructor(){
+        console.log("hi");
+    }
+}
+```
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,12 +3,53 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
-		fontFamily:{
+		fontFamily: {
 			'nextjs': ["Inter", "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "sans-serif"],
-			'sourceCodePro' : ["Source Code Pro", "monospace"],
+			'sourceCodePro': ["Source Code Pro", "monospace"],
 		},
 	},
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+
+					"primary": "#d1d5db",
+
+					"secondary": "#e21f4a",
+
+
+
+					"accent": "#34d399",
+
+
+
+					"neutral": "#331f38",
+
+
+
+					"base-100": "#1c1c1c",
+
+
+
+					"info": "#80a5e0",
+
+
+
+					"success": "#167951",
+
+
+
+					"warning": "#fbdb3c",
+
+
+
+					"error": "#ec6f88",
+				},
+			},
+		],
+	},
 	plugins: [
-		require('@tailwindcss/typography')
+		require('@tailwindcss/typography'),
+		require('daisyui')
 	],
 }
