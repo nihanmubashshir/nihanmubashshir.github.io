db03954e459509bf432f704c43473cd151762c89
Nihan <nihanmubashshir@gmail.com>
2023-09-25 08:09:16 +0600

    seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://nihanmubashshir.github.io',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: "https://nihanmubashshir.github.io",
+  integrations: [mdx(), sitemap(), tailwind()],
+  redirects: {
+    "/": "/blog",
+  },
 });
